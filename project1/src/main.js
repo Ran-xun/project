2953ddbccc9470e6e8cd54c6fd3316b7abe83c58
RanXun <ranxun127@qq.com>
2025-07-31 11:15:45 +0800

    delate
    删除了一些不必要的文件

--- project1/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
-pinia.use(piniaPluginPersistedstate)
-app.mount('#app')
